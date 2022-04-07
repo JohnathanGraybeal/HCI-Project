d1201866ebb9c5be90b5c7e9ebcdb8a2d87b4c53
Johnathan Graybeal <graybealjohnathan@outlook.com>
2022-04-07 01:23:48 -0400

    migration

--- BitWise/BitWise/Migrations/20220407052324_data-protection.Designer.cs
+++ BitWise/BitWise/Migrations/20220407052324_data-protection.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BitWise.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BitWise.Migrations
 {
     [DbContext(typeof(BitWiseContext))]
-    partial class BitWiseContextModelSnapshot : ModelSnapshot
+    [Migration("20220407052324_data-protection")]
+    partial class dataprotection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
